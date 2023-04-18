1b5dd8a7a54e928e5e7153b6255c17556733e4eb
Daler <127861287+denaler@users.noreply.github.com>
2023-04-19 00:46:56 +0700

    Create Convert string to camel case.js

---
+++ Convert string to camel case.js
@@ -0,0 +1,28 @@
+function toCamelCase(str){
+  let string = str.split(/[-,_]/);
+  let result = '';
+  result += string[0];
+  for (let i = 1; string.length > i; i++) {
+    result += string[i].charAt(0).toUpperCase() + string[i].slice(1);
+  } return result;
+}
+
+/* Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(toCamelCase(''), '', "An empty string was provided but not returned")
+    assert.strictEqual(toCamelCase("the_stealth_warrior"), "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value")
+    assert.strictEqual(toCamelCase("The-Stealth-Warrior"), "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value")
+    assert.strictEqual(toCamelCase("A-B-C"), "ABC", "toCamelCase('A-B-C') did not return correct value")
+  });
+}); */
